8779c119ff5db5a9aafc3790993fc584b810ae3b
Hoang Pham <pham.hoang1591@gmail.com>
2020-01-21 11:51:59 +0200

    update ft_list_remove_if.c

--- lv4/ft_list_remove_if/ft_list_remove_if.c
+++ lv4/ft_list_remove_if/ft_list_remove_if.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   ft_list_remove_if.c                                :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: hopham <hopham@student.hive.fi>            +#+  +:+       +#+        */
+/*   By: HoangPham <HoangPham@student.42.fr>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/12/17 13:12:36 by hopham            #+#    #+#             */
-/*   Updated: 2020/01/07 13:37:09 by hopham           ###   ########.fr       */
+/*   Updated: 2020/01/21 11:47:37 by HoangPham        ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,24 +14,24 @@
 
 void	ft_list_remove_if(t_list **begin_list, void *data_ref, int (*cmp)())
 {
-	t_list	*cur;
-	t_list	*tmp;
+	t_list	*remove;
+	t_list	*curr;
 
-	while (*begin_list && cmp((*begin_list)->data, data_ref) == 0)
+	curr = *begin_list;
+	while (curr && curr->next)
 	{
-		cur = *begin_list;
-		*begin_list = (*begin_list)->next;
-		free(cur);
-	}
-	cur = *begin_list;
-	while (cur && cur->next)
-	{
-		if (cmp(cur->next->data, data_ref) == 0)
+		if ((*cmp)(curr->next->data, data_ref) == 0)
 		{
-			tmp = cur->next;
-			cur->next = tmp->next;
-			free(tmp);
+			remove = curr->next;
+			curr->next = curr->next->next;
+			free(remove);
 		}
-		cur = cur->next;
+		curr = curr->next;
+	}
+	curr = *begin_list;
+	if (curr && (*cmp)(curr->data, data_ref) == 0)
+	{
+		*begin_list = curr->next;
+		free(curr);
 	}
 }
